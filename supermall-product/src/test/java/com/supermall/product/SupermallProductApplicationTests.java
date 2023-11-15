eec3b317b3291ddc503ba77d3a4a4a03347de775
dhx <1501218997@qq.com>
2023-11-15 14:32:11 +0800

    项目初始化

---
+++ supermall-product/src/test/java/com/supermall/product/SupermallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
